cd504de0eca4a6b17b9dc8b409c4e432acd9b16e
don mbelembe <donmbelembe@gmail.com>
2018-04-25 09:53:39 +0100

    function to add or remove more than one event listener on a element

---
+++ src/utils.js
@@ -0,0 +1,13 @@
+export default {
+  addEventListeners (el, events, handler) {
+    for (var i = 0, len = events.length; i < len; i++) {
+      el.addEventListener(events[i], handler)
+    }
+  },
+
+  removeEventListeners (el, events, handler) {
+    for (var i = 0, len = events.length; i < len; i++) {
+      el.removeEventListener(events[i], handler)
+    }
+  }
+}
