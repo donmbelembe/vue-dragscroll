6f922f83b40619d3eda8e7021308d24d5c561550
don mbelembe <donmbelembe@gmail.com>
2018-09-15 09:11:53 +0100

    Update packages

--- dist/vue-dragscroll.js
+++ dist/vue-dragscroll.js
@@ -7,7 +7,7 @@
 		exports["VueDragScroll"] = factory();
 	else
 		root["VueDragScroll"] = factory();
-})(this, function() {
+})(typeof self !== 'undefined' ? self : this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
